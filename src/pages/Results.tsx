cbbaac74ff9fb5559b097e42e894ec0c10260f43
mstrYoda <freelancemuhendis@gmail.com>
2025-03-15 00:34:24 +0300

    fix not used erro

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -555,7 +555,7 @@ const Results = () => {
                       dataKey="value"
                       label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
                     >
-                      {companyTypeData.map((entry, index) => (
+                      {companyTypeData.map((_, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                       ))}
                     </Pie>
